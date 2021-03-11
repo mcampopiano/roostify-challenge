03dfec44f6b02bfa8a6427a0cddf110a12e2b45f
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-10 19:19:52 -0600

    Added a button to the applicant.js module which, onClick, generates a window alert. If confirmed, the the deleteApplicant function is called.

--- src/components/applicants/Applicant.js
+++ src/components/applicants/Applicant.js
@@ -1,10 +1,19 @@
 // This component will set how each applicant card will look when rendered to the DOM
-import React from "react"
+import React, { useContext } from "react"
+import { ApplicantContext } from "./ApplicantProvider"
 
-export const Applicant = ({applicant}) => (
-    <div className="applicant">
-        <p>Name: {applicant.firstName} {applicant.lastName}</p>
-        <p>Occupation: {applicant.occupation} </p>
-        <p>SSN: {applicant.SSN} </p>
-    </div>
-)
+export const Applicant = ({ applicant }) => {
+    const {deleteApplicant} = useContext(ApplicantContext)
+
+    return (
+        <div className="applicant">
+            <p>Name: {applicant.firstName} {applicant.lastName}</p>
+            <p>Occupation: {applicant.occupation} </p>
+            <p>SSN: {applicant.SSN} </p>
+            <button onClick={event => {
+                if (window.confirm("Are you sure you want to remove the applicant? This cannot be undone."))
+                deleteApplicant(applicant)
+            }}>Remove applicant</button>
+        </div>
+    )
+}
